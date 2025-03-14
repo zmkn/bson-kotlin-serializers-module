22649010c9e22c07c2441021f840561e830e3e2d
hz <beijingren@vip.qq.com>
2025-03-14 12:28:29 +0800

    调整 ObjectIdSerializer 逻辑

--- src/main/kotlin/com/zmkn/kotlin/serializers/module/bson/serializers/ObjectIdSerializer.kt
+++ src/main/kotlin/com/zmkn/kotlin/serializers/module/bson/serializers/ObjectIdSerializer.kt
@@ -6,16 +6,30 @@ import kotlinx.serialization.descriptors.PrimitiveSerialDescriptor
 import kotlinx.serialization.descriptors.SerialDescriptor
 import kotlinx.serialization.encoding.Decoder
 import kotlinx.serialization.encoding.Encoder
+import kotlinx.serialization.json.*
 import org.bson.types.ObjectId
 
 object ObjectIdSerializer : KSerializer<ObjectId> {
     override val descriptor: SerialDescriptor = PrimitiveSerialDescriptor("ObjectIdSerializer", PrimitiveKind.STRING)
 
     override fun serialize(encoder: Encoder, value: ObjectId) {
-        encoder.encodeString(value.toHexString())
+        if (encoder is JsonEncoder) {
+            val jsonObject = buildJsonObject {
+                put("\$oid", JsonPrimitive(value.toHexString()))
+            }
+            encoder.encodeJsonElement(jsonObject)
+        } else {
+            throw IllegalStateException("This serializer can only encode to JSON.")
+        }
     }
 
     override fun deserialize(decoder: Decoder): ObjectId {
-        return ObjectId(decoder.decodeString())
+        return if (decoder is JsonDecoder) {
+            val jsonObject = decoder.decodeJsonElement().jsonObject
+            val oid = jsonObject["\$oid"]?.jsonPrimitive?.content ?: throw IllegalArgumentException("Invalid ObjectId format.")
+            ObjectId(oid)
+        } else {
+            throw IllegalStateException("This serializer can only decode from JSON")
+        }
     }
 }
